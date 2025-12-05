ff0ddd1e7c9b5399dc5235afc7c57010180c1c87
maminelalle <23097@supnum.mr>
2025-12-05 04:11:27 +0000

    Page problemes et solutions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Acceuil/Navbar';
 import HomePage from './Acceuil/HomePage';
 import NIRDQuiz from './Quiz/NIRDQuiz';
+import NirdPage from './Nird/NirdPage';
 // import DiagnosticPage from './Diagnostic/DiagnosticPage';
 // import ResourcesPage from './Ressources/ResourcesPage';
 
@@ -27,6 +28,9 @@ function App() {
             {/* Autres pages si besoin */}
             {/* <Route path="/diagnostic" element={<DiagnosticPage />} /> */}
             {/* <Route path="/ressources" element={<ResourcesPage />} /> */}
+            
+            {/* Route NIRD */}
+            <Route path="/nird" element={<NirdPage />} />
           </Routes>
         </main>
       </div>
